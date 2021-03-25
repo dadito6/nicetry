1067a31afb44434ca0fe573339ee9dfd064329ca
dadito6 <cesararielrea9@gmail.com>
2021-03-24 23:38:56 -0300

    AL FINNNNNNNNN

---
+++ pruebita.py
@@ -0,0 +1,9 @@
+import random 
+numero1= int(input('Ingrese un numero: '))
+numero2= int(input('Ingrese un numero: '))
+while (numero1 or numero2 != -1):
+    numero1= int(input('Ingrese un numero: '))
+    numero1= int(input('Ingrese un numero: '))
+    suma=numero1+numero2
+    print('la suma de los numeros son:' +suma)
+
